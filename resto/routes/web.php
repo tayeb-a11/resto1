9920738b2e6cc957db7ca78d0b341a9bb40d16ec
tayeb-a11 <125749271+tayeb-a11@users.noreply.github.com>
2025-08-05 18:21:09 +0200

    Update web.php

--- resto/routes/web.php
+++ resto/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes pour la vérification d'abonnement
+Route::prefix('subscription')->group(function () {
+    Route::get('/check', [\App\Http\Controllers\RestoSubscriptionController::class, 'checkSubscription'])->name('subscription.check');
+    Route::get('/status', [\App\Http\Controllers\RestoSubscriptionController::class, 'getSubscriptionStatus'])->name('subscription.status');
+    Route::get('/expired', [\App\Http\Controllers\RestoSubscriptionController::class, 'redirectToExpiredPage'])->name('subscription.expired');
+});
